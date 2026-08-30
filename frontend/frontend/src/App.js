bfed6b7d2f437fe1b61d30bf3a0f3dc8f5f559e4
Andrea Pineda <andreapineda1245@gmail.com>
2026-08-30 12:40:37 -0600

    Corregir estructura del frontend

---
+++ frontend/frontend/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh', margin: 0, padding: 0, overflow: 'hidden' }}>
+      <iframe 
+        src="http://localhost:5000" 
+        style={{ width: '100%', height: '100%', border: 'none' }}
+        title="Joyas Valentina"
+      />
+    </div>
+  );
+}
+
+export default App;
